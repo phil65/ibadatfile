9fc18a3e8cc364305075ec2ed2870c4d96a3e0a2
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-05-01 20:23:09 +0200

    chore: cleanup

--- ibadatfile/ibadatfile.py
+++ ibadatfile/ibadatfile.py
@@ -1,6 +1,9 @@
-# -*- coding: utf-8 -*-
+from __future__ import annotations
 
+import os
+from typing import Generator
 import datetime
+import pathlib
 
 import numpy as np
 import pandas as pd
@@ -13,189 +16,54 @@ B = 0
 V = client.VARIANT(pythoncom.VT_BYREF | pythoncom.VT_VARIANT, 2)
 
 
-class IbaDatFile(object):
-    """
-    Class representing an Iba .dat file
-    """
-
-    def __init__(self, path=None, raw_mode: bool = False, preload: bool = True):
-        """
-        initialize the dat file object
-        """
-        self.path = path
-        try:
-            self.reader = client.dynamic.Dispatch("IbaFilesLite.IbaFile")
-        except pywintypes.com_error:
-            raise IOError("Necessary dlls are not installed.")
-        self.reader.PreLoad = int(preload)
-        self.reader.RawMode = int(raw_mode)
-
-    def __enter__(self):
-        """
-        magic method for context manager
-        """
-        self.reader.Open(self.path)
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        """
-        magic method for context manager
-        """
-        self.reader.Close()
-
-    def __iter__(self):
-        """
-        iterator interface, returns next channel
-        """
-        enumerator = self.reader.EnumChannels()
-        while not enumerator.IsAtEnd():
-            channel = enumerator.Next()
-            yield IbaChannel(channel)
-
-    def open(self, path):
-        """
-        opens .dat file from *path
-        """
-        self.reader.Open(path)
-        self.path = path
-        return self
-
-    def close(self):
-        """
-        closes reader
-        """
-        self.reader.Close()
-
-    def index(self) -> pd.DatetimeIndex:
-        """
-        returns the time index for the channels
-        """
-        start = self.starttime()
-        frames = self.frames()
-        clk = self.clk()
-        times = [start + datetime.timedelta(seconds=i * clk)
-                 for i in range(frames)]
-        return pd.DatetimeIndex(times, name="time")
-
-    def frames(self) -> int:
-        """
-        returns amount of frames of channels
-        """
-        return int(self.reader.QueryInfoByName("frames"))
-
-    def signal_count(self) -> int:
-        """
-        returns amount of channels
-        """
-        return int(self.reader.QueryInfoByName("totalSignalCount"))
-
-    def clk(self) -> float:
-        """
-        returns the clock rate
-        """
-        return float(self.reader.QueryInfoByName("clk"))
-
-    def recorder_version(self):
-        """
-        returns the software version of the recorder
-        """
-        return self.reader.QueryInfoByName("version")
-
-    def recorder_name(self) -> str:
-        """
-        returns the name of the recorder
-        """
-        return self.reader.QueryInfoByName("name")
-
-    def recorder_type(self) -> str:
-        """
-        returns the software version of the recorder
-        """
-        return self.reader.QueryInfoByName("type")
-
-    def starttime(self) -> str:
-        """
-        returns the recording start time as datetime object
-        """
-        return datetime.datetime.strptime(self.reader.QueryInfoByName("starttime"),
-                                          '%d.%m.%Y %H:%M:%S.%f')
-
-    def starttime_as_str(self) -> str:
-        """
-        returns the recording start time as datetime object
-        """
-        return self.reader.QueryInfoByName("starttime")
-
-    def return_channel_names(self) -> list:
-        return [channel.name() for channel in self]
-
-    def data(self) -> pd.DataFrame:
-        """
-        returns data as a dataframe
-        """
-        data = {channel.name(): channel.data() for channel in self}
-        df = pd.DataFrame.from_dict(data)
-        df.index = self.index()
-        return df
+def read_ibadat(
+    path: os.PathLike,
+    raw_mode: bool = False,
+    preload: bool = True,
+) -> pd.DataFrame:
+    with IbaDatFile(path, raw_mode, preload) as file:
+        return file.data()
 
 
-class IbaChannel(object):
+class IbaChannel:
     """
     Class representing a single channel of an iba .dat file
     """
 
     def __init__(self, channel):
-        """
-        initialize the channel object
-        """
+        """Initialize the channel object."""
         self.channel = channel
 
     def name(self) -> str:
-        """
-        returns the channel name
-        """
+        """Return the channel name."""
         return self.channel.QueryInfoByName("name")
 
     def minscale(self):
-        """
-        returns the channel minscale
-        """
+        """Return the channel minscale."""
         return self.channel.QueryInfoByName("minscale")
 
     def maxscale(self):
-        """
-        returns the channel maxscale
-        """
+        """Return the channel maxscale."""
         return self.channel.QueryInfoByName("maxscale")
 
     def xoffset(self) -> int:
-        """
-        returns the channel x offset (in frames?)
-        """
+        """Return the channel x offset (in frames?)."""
         return self.channel.QueryInfoByName("xoffset")
 
     def unit(self) -> str:
-        """
-        returns unit of the channel data
-        """
+        """Return unit of the channel data."""
         return self.channel.QueryInfoByName("unit")
 
     def digchannel(self):
-        """
-        returns digchannel info
-        """
+        """Return digchannel info."""
         return self.channel.QueryInfoByName("digchannel")
 
     def pda_type(self) -> str:
-        """
-        returns the channel unit
-        """
+        """Return the channel unit."""
         return self.channel.QueryInfoByName("$PDA_Typ")
 
-    def data(self):
-        """
-        returns the channel data
-        """
+    def data(self) -> np.array:
+        """Return the channel data."""
         if self.channel.IsDefaultTimebased():
             data = np.array(self.channel.QueryTimebasedData(A, B, V)[2])
         else:
@@ -208,19 +76,113 @@ class IbaChannel(object):
             return data
 
     def is_bool(self) -> bool:
-        """
-        returns true if series contains boolean values
-        """
+        """Return true if series contains boolean values."""
         return self.channel.IsDigital()
 
     def is_time_based(self) -> bool:
-        """
-        returns bool whether series is time based
-        """
+        """Return bool whether series is time based."""
         return self.channel.IsDefaultTimebased()
 
     def id(self) -> int:
-        """
-        returns the channel id
-        """
+        """Return the channel id."""
         return self.channel.QueryChannelId()
+
+
+class IbaDatFile:
+    """
+    Class representing an Iba .dat file
+    """
+
+    def __init__(
+        self,
+        path: os.PathLike,
+        raw_mode: bool = False,
+        preload: bool = True,
+    ):
+        """Initialize the dat file object."""
+        self.path = os.fspath(path)
+        try:
+            self.reader = client.dynamic.Dispatch("IbaFilesLite.IbaFile")
+        except pywintypes.com_error as e:
+            raise IOError("Necessary dlls are not installed.") from e
+        self.reader.PreLoad = int(preload)
+        self.reader.RawMode = int(raw_mode)
+
+    def __enter__(self):
+        """Magic method for context manager."""
+        self.reader.Open(self.path)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """Magic method for context manager."""
+        self.reader.Close()
+
+    def __iter__(self) -> Generator[IbaChannel, None, None]:
+        """Iterator interface, returns next channel."""
+        enumerator = self.reader.EnumChannels()
+        while not enumerator.IsAtEnd():
+            channel = enumerator.Next()
+            yield IbaChannel(channel)
+
+    def open(self, path: os.PathLike):
+        """Open .dat file from *path."""
+        self.path = os.fspath(path)
+        self.reader.Open(self.path)
+        return self
+
+    def close(self):
+        """Close reader."""
+        self.reader.Close()
+
+    def index(self) -> pd.DatetimeIndex:
+        """Return the time index for the channels."""
+        start = self.starttime()
+        frames = self.frames()
+        clk = self.clk()
+        times = [start + datetime.timedelta(seconds=i * clk) for i in range(frames)]
+        return pd.DatetimeIndex(times, name="time")
+
+    def frames(self) -> int:
+        """Return amount of frames of channels."""
+        return int(self.reader.QueryInfoByName("frames"))
+
+    def signal_count(self) -> int:
+        """Return amount of channels."""
+        return int(self.reader.QueryInfoByName("totalSignalCount"))
+
+    def clk(self) -> float:
+        """Return the clock rate."""
+        return float(self.reader.QueryInfoByName("clk"))
+
+    def recorder_version(self):
+        """Return the software version of the recorder."""
+        return self.reader.QueryInfoByName("version")
+
+    def recorder_name(self) -> str:
+        """Return the name of the recorder."""
+        return self.reader.QueryInfoByName("name")
+
+    def recorder_type(self) -> str:
+        """Return the software version of the recorder."""
+        return self.reader.QueryInfoByName("type")
+
+    def starttime(self) -> datetime.datetime:
+        """Return the recording start time as datetime object."""
+        return datetime.datetime.strptime(
+            self.reader.QueryInfoByName("starttime"), "%d.%m.%Y %H:%M:%S.%f"
+        )
+
+    def starttime_as_str(self) -> str:
+        """Return the recording start time as str."""
+        return self.reader.QueryInfoByName("starttime")
+
+    def return_channel_names(self) -> list[str]:
+        """Return list of channel names."""
+        return [channel.name() for channel in self]
+
+    def data(self) -> pd.DataFrame:
+        """Return data as a dataframe."""
+        data = {channel.name(): channel.data() for channel in self}
+        df = pd.DataFrame.from_dict(data)
+        df.index = self.index()
+        return df
