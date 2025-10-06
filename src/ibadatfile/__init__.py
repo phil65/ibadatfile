fb3ccced9295b30ad74d3c527d246ae67653ab01
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-10-06 18:07:26 +0200

    chore: bump template

--- src/ibadatfile/__init__.py
+++ src/ibadatfile/__init__.py
@@ -1,8 +1,17 @@
+"""IbaDatFile: Pythonic wrapper for IBA dat file DLL"""
+
 from __future__ import annotations
 
 from importlib.metadata import version
 
 __version__ = version("ibadatfile")
+__title__ = "IbaDatFile"
+__description__ = "Pythonic wrapper for IBA dat file DLL"
+__author__ = "Philipp Temminghoff"
+__author_email__ = "philipptemminghoff@googlemail.com"
+__copyright__ = "Copyright (c) 2025 Philipp Temminghoff"
+__license__ = "MIT"
+__url__ = "https://github.com/phil65/ibadatfile"
 
 from .ibadatfile import IbaChannel, IbaDatFile, read_ibadat
 
